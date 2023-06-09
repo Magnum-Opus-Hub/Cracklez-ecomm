b7d30932628984bc56c24871a2fac64330b1b258
FilipTheGod <filipstan666@gmail.com>
2023-06-09 16:04:19 +0300

    done template for crcklez

--- components/providers.tsx
+++ components/providers.tsx
@@ -15,7 +15,7 @@ export function Providers({ children }: Props) {
   currency="RON"
   shouldPersist
   cartMode="checkout-session"
-  stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
+  stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!}
   >
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Toaster />
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,6 @@ export const client = createClient({
   apiVersion,
   dataset,
   projectId,
+  token: "skP6WGauYBWMWbKMURDgGWUaR2XH8guNGw5KSGZTKYw31aiDpmBwQL50CFkblFmNd8tw8p2dQpGgMG4ghfHD92892zC8w7FE0OBW9LIMVBhQUalv0bs4TjjacQMoVblMl5zXaMCReDF8iOJ9rfAOOEJpJM55LQVEjkOxApvsuAUzVDr808Ki",
   useCdn,
 })
